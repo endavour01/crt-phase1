2906df7b12161a91bace3e174e351a3615e044bc
Vikalp Soni <imvikalpsoni@gmail.com>
2026-05-06 23:52:02 +0530

    commiting all files

---
+++ Day-6/hw.cpp
@@ -0,0 +1,2 @@
+//WAP C++ to create a student management system using parameterized constructor. The system should allow adding students, displaying student details,
+//name, rollno, regno,email,phno,marks/grade
