fe303a59fc6bdff956cd287a0781f3a5a316d4d5
Vikalp Soni <imvikalpsoni@gmail.com>
2026-05-07 10:57:48 +0530

    heirarchial inheritance

---
+++ Day-7/heirarchial_inheritance.cpp
@@ -0,0 +1,51 @@
+#include  <iostream>
+using namespace std;
+class Player
+{
+public:
+    string name;
+    void setName(string n)
+    {
+        name = n;
+    }
+};
+class Cricketer : public Player
+{
+public:
+    int runs;
+    void setruns(int r)
+    {
+        runs = r;
+    }
+    void show()
+    {
+        cout << "Name: " << name << endl;
+        cout << "Runs: " << runs << endl;
+    }
+};
+    class footballer : public Player
+    {
+    public:
+        int goals;
+        void setgoals(int g)
+        {
+            goals = g;
+        }
+        void show() 
+        {
+            cout << "Name: " << name << endl;
+            cout << "Goals: " << goals << endl;
+        }
+    };
+int main()
+{
+    Cricketer c1;
+    c1.setName("Virat");
+    c1.setruns(12000);
+    c1.show();
+    footballer f1;
+    f1.setName("Ronaldo");
+    f1.setgoals(800);
+    f1.show();
+    return 0;
+}
